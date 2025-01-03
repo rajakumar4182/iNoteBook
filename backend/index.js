a900966344b5ef6cea7ef911032fa5bd2511df3b
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2025-01-03 08:40:32 +0530

    Added module and routes

--- backend/index.js
+++ backend/index.js
@@ -5,10 +5,14 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//Available Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
