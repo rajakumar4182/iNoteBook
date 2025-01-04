65bd79f65e52cf4c2bfbf6b3ba30123fbc945980
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2025-01-04 14:30:14 +0530

    Added validation on user

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-const mongoURI = "mongodb+srv://raja4182:Kvk210raja%40@test.s0drx.mongodb.net/";
+const mongoURI =
+  "mongodb+srv://raja4182:Kvk210raja%40@test.s0drx.mongodb.net/inotebook";
 
 const connectToMongo = async () => {
   await mongoose.connect(mongoURI);
