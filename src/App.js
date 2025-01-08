614fcf1b0be211c4ff97b61d286d256a2321d321
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2025-01-08 21:32:28 +0530

    created component and setup route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { Home } from "./components/Home";
+import { About } from "./components/About";
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+
+        <Route exact path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
